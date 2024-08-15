988db13edce394b7149dd2e7bd23d3bf09ddc85c
Lightzin4521 <110855787+Lightzin4521@users.noreply.github.com>
2024-08-15 11:02:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.text-resultado');
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1", "Alternativa 2"]
+        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual e o seu primeiro pensamento?",
+        alternativas: ["isso é assustador", "isso é maravilhoso!"]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1", "Alternativa 2"]
+        texto: "Alternativa 1",
+        afirmação: ["Afirmação da alternativa 2"]
     },
+    {
+        texto: "Alternativa 2",
+        afirmação: "Afirmação da alternativa 2"
+    }
 ];
 let atual = 0;
 let perguntaAtual;
@@ -19,4 +23,15 @@ function mostraPergunta() {
     perguntaAtual = pergunta[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
-mostraPergunta ();
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",  
+        function () {
+            atual++;
+            mostraPergunta();
+        });
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
